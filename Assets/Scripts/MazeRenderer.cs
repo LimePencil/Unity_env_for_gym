5232ed063010873b2a7329f505e872de0946d461
Jaeyoung <71117066+JaeyoungShin2003@users.noreply.github.com>
2021-08-02 17:12:23 +0900

    more changes

--- Assets/Scripts/MazeRenderer.cs
+++ Assets/Scripts/MazeRenderer.cs
@@ -25,25 +25,28 @@ public class MazeRenderer : MonoBehaviour
     [SerializeField]
     private Transform playerPrefab = null;
     [SerializeField]
-    public bool loadPrevPrefab = false;
+    public bool loadPrev = false;
+    public bool isPrefab = false;
     [SerializeField]
     WallState[,] theMaze;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(loadPrevPrefab == false)
+        if (!isPrefab)
         {
-            theMaze = MazeGenerator.Generate(width, height);
-            Draw(theMaze);
-            ObjectSerialize(theMaze,"Assets/Maze","mazeV1","cool");
+            if (loadPrev == false)
+            {
+                theMaze = MazeGenerator.Generate(width, height);
+                Draw(theMaze);
+                ObjectSerialize(theMaze, "Assets/Maze", "mazeV1", "cool");
+            }
+            else
+            {
+                theMaze = DeSerialization("Assets/Maze", "mazeV1", "cool");
+                Draw(theMaze);
+            }
         }
-        else
-        {
-            theMaze = DeSerialization("Assets/Maze", "mazeV1", "cool");
-            Draw(theMaze);  
-        }
-
     }
 
     public static void ObjectSerialize(object obj,string path, string filename, string ext)
@@ -74,9 +77,9 @@ public class MazeRenderer : MonoBehaviour
     private void Draw(WallState[,] maze)
     {
 
-        var floor = Instantiate(floorPrefab, transform);
-        Instantiate(playerPrefab,transform);
-        floor.localScale = new Vector3(width,1, height);
+        var floor = Instantiate(floorPrefab, transform, false);
+        Instantiate(playerPrefab,transform,false);
+        floor.localScale = new Vector3(width/4,1, height/4);
 
         for (int i = 0; i < width; ++i)
         {
@@ -88,14 +91,14 @@ public class MazeRenderer : MonoBehaviour
                 if (cell.HasFlag(WallState.UP))
                 {
                     var topWall = Instantiate(wallPrefab, transform) as Transform;
-                    topWall.position = position + new Vector3(0, 0, size / 2);
+                    topWall.localPosition = position + new Vector3(0, 0, size / 2);
                     topWall.localScale = new Vector3(size, topWall.localScale.y, topWall.localScale.z);
                 }
 
                 if (cell.HasFlag(WallState.LEFT))
                 {
                     var leftWall = Instantiate(wallPrefab, transform) as Transform;
-                    leftWall.position = position + new Vector3(-size / 2, 0, 0);
+                    leftWall.localPosition = position + new Vector3(-size / 2, 0, 0);
                     leftWall.localScale = new Vector3(size, leftWall.localScale.y, leftWall.localScale.z);
                     leftWall.eulerAngles = new Vector3(0, 90, 0);
                 }
@@ -105,7 +108,7 @@ public class MazeRenderer : MonoBehaviour
                     if (cell.HasFlag(WallState.RIGHT))
                     {
                         var rightWall = Instantiate(wallPrefab, transform) as Transform;
-                        rightWall.position = position + new Vector3(+size / 2, 0, 0);
+                        rightWall.localPosition = position + new Vector3(+size / 2, 0, 0);
                         rightWall.localScale = new Vector3(size, rightWall.localScale.y, rightWall.localScale.z);
                         rightWall.eulerAngles = new Vector3(0, 90, 0);
                     }
@@ -116,7 +119,7 @@ public class MazeRenderer : MonoBehaviour
                     if (cell.HasFlag(WallState.DOWN))
                     {
                         var bottomWall = Instantiate(wallPrefab, transform) as Transform;
-                        bottomWall.position = position + new Vector3(0, 0, -size / 2);
+                        bottomWall.localPosition = position + new Vector3(0, 0, -size / 2);
                         bottomWall.localScale = new Vector3(size, bottomWall.localScale.y, bottomWall.localScale.z);
                     }
                 }
--- Assets/Scripts/MoveToGoalScrip.cs
+++ Assets/Scripts/MoveToGoalScrip.cs
@@ -20,26 +20,29 @@ public class MoveToGoalScrip : Agent
     public GameObject goalPrefab;
     private GameObject goalClone = null;
     MazeRenderer render;
+    Transform parentTransform;
     //초기화 작업을 위해 한번 호출되는 메소드
 
     public override void Initialize()
     {
         tr = GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
-        render = FindObjectOfType<MazeRenderer>();
-        goalClone = (GameObject)Instantiate(goalPrefab, new Vector3(((render.width- 1) / 2 * (Random.Range(0, 2) * 2 - 1)), 0.25f, ((render.height- 1) / 2) * (Random.Range(0, 2) * 2 - 1)), Quaternion.identity);
+        render = GetComponentInParent<MazeRenderer>();
+        parentTransform = GetComponentInParent<MazeRenderer>().transform;
+        goalClone = (GameObject)Instantiate(goalPrefab, parentTransform.position + new Vector3(((render.width- 1) / 2 * (Random.Range(0, 2) * 2 - 1)), 0.25f, ((render.height- 1) / 2) * (Random.Range(0, 2) * 2 - 1)), Quaternion.identity,parentTransform);
         //originMt = floorRender.material;
     }
 
     //에피소드(학습단위)가 시작할때마다 호출
     public override void OnEpisodeBegin()
     {
-        Destroy(goalClone);
-        goalClone = (GameObject)Instantiate(goalPrefab, new Vector3(((render.width- 1) / 2 * (Random.Range(0, 2) * 2 - 1)), 0.25f, ((render.height- 1) / 2) * (Random.Range(0, 2) * 2 - 1)), Quaternion.identity);
+        
         //물리력을 초기화
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
-        tr.position = Vector3.zero;
+        tr.localPosition = new Vector3(0, 1, 0);
+        Destroy(goalClone);
+        goalClone = (GameObject)Instantiate(goalPrefab, parentTransform.position + new Vector3(((render.width - 1) / 2 * (Random.Range(0, 2) * 2 - 1)), 0.25f, ((render.height - 1) / 2) * (Random.Range(0, 2) * 2 - 1)), Quaternion.identity, parentTransform);
         //StartCoroutine(RevertMaterial());
     }
 
@@ -63,8 +66,8 @@ public class MoveToGoalScrip : Agent
         float h = Mathf.Clamp(actionBuffers.ContinuousActions[0], -1.0f, 1.0f);
         float v = Mathf.Clamp(actionBuffers.ContinuousActions[1], -1.0f, 1.0f);
         Vector3 dir = (Vector3.forward * v);
-        rb.AddRelativeForce(dir.normalized * 10.0f);
-        tr.Rotate(Vector3.up * 5.0f*h);
+        tr.Translate(dir * 0.005f);
+        tr.Rotate(Vector3.up * 0.1f*h);
         //지속적으로 이동을 이끌어내기 위한 마이너스 보상
         SetReward(-0.001f);
     }
